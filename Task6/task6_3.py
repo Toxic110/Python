1c6720c50fbbd569cb2ec38492a461206e766c9e
Максим Воробьев <maksimvorobev@MacBook-Air.local>
2023-06-04 09:15:48 +0400

    Поправил задание 6_3

--- Task6/task6_3.py
+++ Task6/task6_3.py
@@ -2,8 +2,9 @@ a = int(input())
 b = int(input())
 arr = []
 
-for i in range(a, b):
+for i in range(a, b + 1):
     if (i % 2 == 0):
+        print(i)
         arr.append(i)
 
 print(' '.join(str(x) for x in arr))
